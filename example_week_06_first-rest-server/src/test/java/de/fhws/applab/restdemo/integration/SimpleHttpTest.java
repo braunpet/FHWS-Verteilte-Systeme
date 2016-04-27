c7ddddeadaa83d7c19ab13cc1dbcb841251099bd
Peter Braun <mail@peter-braun.org>
2016-04-27 11:32:43 +0200

    Ignore test case

--- example_week_06_first-rest-server/src/test/java/de/fhws/applab/restdemo/integration/SimpleHttpTest.java
+++ example_week_06_first-rest-server/src/test/java/de/fhws/applab/restdemo/integration/SimpleHttpTest.java
@@ -1,11 +1,5 @@
 package de.fhws.applab.restdemo.integration;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -13,29 +7,39 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
-public class SimpleHttpTest {
+import static org.junit.Assert.assertEquals;
+
+public class SimpleHttpTest
+{
 
 	protected String url;
 
 	@Before
-	public void startup() {
+	public void startup( )
+	{
 		this.url = "http://localhost:8080/demo";
 	}
 
 	@Test
-	public void test1() throws Exception {
+	@Ignore
+	public void test1( ) throws Exception
+	{
 
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet(this.url + "/api/accounts");
-		CloseableHttpResponse response1 = httpclient.execute(httpGet);
+		final CloseableHttpClient httpclient = HttpClients.createDefault( );
+		final HttpGet httpGet = new HttpGet( this.url + "/api/accounts" );
+		final CloseableHttpResponse response1 = httpclient.execute( httpGet );
 
-		try {
-			StatusLine status = response1.getStatusLine();
-			assertEquals(HttpStatus.SC_OK, status.getStatusCode());
-		} finally {
-			response1.close();
+		try
+		{
+			final StatusLine status = response1.getStatusLine( );
+			assertEquals( HttpStatus.SC_OK, status.getStatusCode( ) );
+		}
+		finally
+		{
+			response1.close( );
 		}
 	}
 
